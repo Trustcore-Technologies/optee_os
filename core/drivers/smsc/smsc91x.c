fc0982c467eff4125f9fb98dd00b40ec4bb08883
Rami Neiman <rneiman@trustcoretechnologies.com>
2022-11-11 14:55:58 -0500

    Added foundation-v8.dts Sample smc driver linking to the new node

--- core/drivers/smsc/smsc91x.c
+++ core/drivers/smsc/smsc91x.c
@@ -8,26 +8,33 @@
 
 #include <assert.h>
 #include <drivers/smsc/smsc91x.h>
-#include <drivers/rtc.h>
-#include <io.h>
+#include <config.h>
+#include <initcall.h>
 #include <kernel/dt.h>
+#include <kernel/dt_driver.h>
+#include <libfdt.h>
+#include <malloc.h>
+#include <sys/queue.h>
+#include <tee_api_defines_extensions.h>
+#include <tee_api_types.h>
 
 
-static int
-smsc91x_probe(const void *fdt __unused, struct device *dev, const void *data) {
+static TEE_Result
+smsc91x_probe(const void *fdt __unused, int dev, const void *data) {
 	EMSG("SMSC bitches!!!!!");
 	return 0;
 }
 
 
 static const struct dt_device_match smsc91x_match_table[] = {
-	{ .compatible = "smsc" },
+	{ .compatible = "smc91x", },
 	{ }
 };
 
-DEFINE_DT_DRIVER(smsc91x_dt_driver) = {
-	.name = "smc",
+DEFINE_DT_DRIVER(smc_dt_driver) = {
+	.name = "smc91x",
 	.type = DT_DRIVER_NOTYPE,
 	.match_table = smsc91x_match_table,
 	.probe = smsc91x_probe,
 };
+
