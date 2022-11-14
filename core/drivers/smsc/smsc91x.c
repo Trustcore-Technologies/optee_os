fc60691fd23788aae3f58febee8ca9375120d233
Rami Neiman <rneiman@trustcoretechnologies.com>
2022-11-13 20:07:26 -0500

    Successful card probe

--- core/drivers/smsc/smsc91x.c
+++ core/drivers/smsc/smsc91x.c
@@ -17,14 +17,157 @@
 #include <sys/queue.h>
 #include <tee_api_defines_extensions.h>
 #include <tee_api_types.h>
+#include <drivers/smsc/errno.h>
+#include <io.h>
+
+#ifndef SMC_NOWAIT
+# define SMC_NOWAIT		0
+#endif
+
+struct platform_device_id {
+    char name[20];
+    unsigned long driver_data;
+};
+
+static struct smc_local * smsc = NULL;
+static int nowait = SMC_NOWAIT;
+static const char version[] =
+	"smc91x.c: v1.1, sep 22 2004 by Nicolas Pitre <nico@fluxnic.net>\n";
+static const char * chip_ids[ 16 ] =  {
+	NULL, NULL, NULL,
+	/* 3 */ "SMC91C90/91C92",
+	/* 4 */ "SMC91C94",
+	/* 5 */ "SMC91C95",
+	/* 6 */ "SMC91C96",
+	/* 7 */ "SMC91C100",
+	/* 8 */ "SMC91C100FD",
+	/* 9 */ "SMC91C11xFD",
+	NULL, NULL, NULL,
+	NULL, NULL, NULL};
+
+static TEE_Result init_eth();
+static TEE_Result smc_probe(void  *ioaddr);
 
 
 static TEE_Result
 smsc91x_probe(const void *fdt __unused, int dev, const void *data) {
-	EMSG("SMSC bitches!!!!!");
-	return 0;
+	EMSG("SMSC probe()");
+
+	const struct platform_device_id *smsc_id_data;
+	int ret = 0;
+
+	smsc = malloc(sizeof(struct smc_local));
+	if (!smsc)
+		return -ENOMEM;
+
+	smsc_id_data = data;
+
+	EMSG("SMSC device name: %s", smsc_id_data->name);
+
+	paddr_t pa = _fdt_reg_base_address(fdt, 1);
+	size_t size = 0;
+
+	ret = dt_map_dev(fdt, dev, &pa, &size, DT_MAP_SECURE);
+
+	EMSG("SMSC device addr: %lx, %i %x", pa, ret, size);
+
+	smsc->cfg.flags |= (SMC_CAN_USE_8BIT)  ? SMC91X_USE_8BIT  : 0;
+	smsc->cfg.flags |= (SMC_CAN_USE_16BIT) ? SMC91X_USE_16BIT : 0;
+	smsc->cfg.flags |= (SMC_CAN_USE_32BIT) ? SMC91X_USE_32BIT : 0;
+	smsc->cfg.flags |= (nowait) ? SMC91X_NOWAIT : 0;
+
+	smsc->io_shift = SMC91X_IO_SHIFT(smsc->cfg.flags);
+
+	ret = smc_probe(pa);
+
+
+	init_eth();
+
+	return ret;
 }
 
+static TEE_Result smc_probe(void  *ioaddr)
+{
+	static int version_printed = 0;
+	int retval;
+	unsigned int val, revision_register;
+	const char *version_string;
+
+	DMSG("%s: %s", CARDNAME, __func__);
+
+	/* First, see if the high byte is 0x33 */
+	val = SMC_CURRENT_BANK(smsc);
+	DMSG("%s: bank signature probe returned 0x%04x", CARDNAME, val);
+	if ((val & 0xFF00) != 0x3300) {
+		if ((val & 0xFF) == 0x33) {
+			DMSG(
+				"%s: Detected possible byte-swapped interface"
+				" at IOADDR %p", CARDNAME, ioaddr);
+		}
+		retval = -ENODEV;
+		goto err_out;
+	}
+
+	/*
+	 * The above MIGHT indicate a device, but I need to write to
+	 * further test this.
+	 */
+	SMC_SELECT_BANK(smsc, 0);
+	val = SMC_CURRENT_BANK(smsc);
+	if ((val & 0xFF00) != 0x3300) {
+		retval = -ENODEV;
+		goto err_out;
+	}
+
+	/*
+	 * well, we've already written once, so hopefully another
+	 * time won't hurt.  This time, I need to switch the bank
+	 * register to bank 1, so I can access the base address
+	 * register
+	 */
+	SMC_SELECT_BANK(smsc, 1);
+	val = SMC_GET_BASE(smsc);
+	val = ((val & 0x1F00) >> 3) << SMC_IO_SHIFT;
+	if (((unsigned long)ioaddr & (0x3e0 << SMC_IO_SHIFT)) != val) {
+		DMSG("%s: IOADDR %p doesn't match configuration (%x).",
+			CARDNAME, ioaddr, val);
+	}
+
+	/*
+	 * check if the revision register is something that I
+	 * recognize.  These might need to be added to later,
+	 * as future revisions could be added.
+	 */
+	SMC_SELECT_BANK(smsc, 3);
+	revision_register = SMC_GET_REV(smsc);
+	DMSG("%s: revision = 0x%04x", CARDNAME, revision_register);
+	version_string = chip_ids[ (revision_register >> 4) & 0xF];
+	if (!version_string || (revision_register & 0xff00) != 0x3300) {
+		/* I don't recognize this chip, so... */
+		DMSG("%s: IO %p: Unrecognized revision register 0x%04x"
+			", Contact author.", CARDNAME,
+			ioaddr, revision_register);
+
+		retval = -ENODEV;
+		goto err_out;
+	}
+
+	/* At this point I'll assume that the chip is an SMC91x. */
+	if (version_printed++ == 0)
+		DMSG("%s", version);
+
+
+	return TEE_SUCCESS;
+
+	err_out:
+		EMSG( "SMC probe failed");
+		return retval;
+}
+
+TEE_Result init_eth()
+{
+	return TEE_SUCCESS;
+}
 
 static const struct dt_device_match smsc91x_match_table[] = {
 	{ .compatible = "smc91x", },
---
+++ core/include/drivers/smsc/utils.h
@@ -0,0 +1,119 @@
+#ifndef BSTGW_FEC_UTILS_H
+#define BSTGW_FEC_UTILS_H
+
+#include <stdlib.h>
+#include <mm/core_mmu.h>
+
+/*
+ * Branch prediction macros.
+ */
+
+#ifndef __predict_true
+#define	__predict_true(x)	__builtin_expect(!!(x), 1)
+#define	__predict_false(x)	__builtin_expect(!!(x), 0)
+#endif
+
+/*
+ * Various C helpers and attribute macros.
+ */
+
+#ifndef __unused
+#define	__unused		__attribute__((__unused__))
+#endif
+
+#ifndef __arraycount
+#define	__arraycount(__x)	(sizeof(__x) / sizeof(__x[0]))
+#endif
+
+static void bstgw_spin_cycles(unsigned int *count) {
+    volatile unsigned int n = *count;
+    while(__predict_true( n-- )) __asm volatile("yield" ::: "memory");
+}
+
+#ifndef unlikely
+#define unlikely(x)      __builtin_expect(!!(x), 0)
+#endif
+
+#ifndef likely
+#define likely(x)      __builtin_expect(!!(x), 1)
+#endif
+
+// taken from linux/err.h
+#define MAX_ERRNO   4095
+#define IS_ERR_VALUE(x) unlikely((unsigned long)(void *)(x) >= (unsigned long)-MAX_ERRNO)
+static inline bool IS_ERR(const void *ptr) {
+    return IS_ERR_VALUE((unsigned long)ptr);
+}
+
+// taken from linux/kernel.h
+/*
+ * This looks more complex than it should be. But we need to
+ * get the type for the ~ right in round_down (it needs to be
+ * as wide as the result!), and we want to evaluate the macro
+ * arguments just once each.
+ */
+#define __round_mask(x, y) ((__typeof__(x))((y)-1))
+#define round_up(x, y) ((((x)-1) | __round_mask(x, y))+1)
+#define round_down(x, y) ((x) & ~__round_mask(x, y))
+
+// linux/slab.h
+#define GFP_KERNEL (0)
+static inline void linux_kfree(void *p) { free(p); }
+static inline void *linux_kmalloc(size_t size, unsigned flags __unused) { return malloc(size); }
+static inline void *linux_kzalloc(size_t size, unsigned flags __unused) { return calloc(1, size); }
+
+/* helper functions */
+struct ifnet;
+struct ifnet * bstgw_alloc_etherdev(size_t);
+
+struct device;
+struct resource;
+enum resource_type;
+
+struct resource * bstgw_platform_get_resource(
+    struct device *, enum resource_type, unsigned int);
+
+vaddr_t bstgw_devm_ioremap_resource(struct resource *);
+
+
+#if defined(ARM32)
+#define BITS_PER_LONG 32
+#elif defined(ARM64)
+#define BITS_PER_LONG 64
+#else
+#error Unknown BITS_PER_LONG
+#endif
+
+// TODO: from Linux kernel __fls.h
+static __always_inline unsigned long __fls(unsigned long word)
+{
+	int num = BITS_PER_LONG - 1;
+
+#if BITS_PER_LONG == 64
+	if (!(word & (~0ul << 32))) {
+		num -= 32;
+		word <<= 32;
+	}
+#endif
+	if (!(word & (~0ul << (BITS_PER_LONG-16)))) {
+		num -= 16;
+		word <<= 16;
+	}
+	if (!(word & (~0ul << (BITS_PER_LONG-8)))) {
+		num -= 8;
+		word <<= 8;
+	}
+	if (!(word & (~0ul << (BITS_PER_LONG-4)))) {
+		num -= 4;
+		word <<= 4;
+	}
+	if (!(word & (~0ul << (BITS_PER_LONG-2)))) {
+		num -= 2;
+		word <<= 2;
+	}
+	if (!(word & (~0ul << (BITS_PER_LONG-1))))
+		num -= 1;
+	return num;
+}
+
+#endif /* BSTGW_FEC_UTILS_H */
