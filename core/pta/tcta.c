f97f760e02b13ac9461376e047f708a927fc6237
mkanalatc <117786186+mkanalatc@users.noreply.github.com>
2022-11-21 17:45:29 -0500

    SMC message handling for messages from XVISOR (#4)
    * SMC message handling for messages from XVISOR

--- core/pta/tcta.c
+++ core/pta/tcta.c
@@ -1,5 +1,9 @@
 
 #include <kernel/pseudo_ta.h>
+#include <mm/core_mmu.h>
+#include <sm/optee_smc.h>
+#include <optee_msg.h>
+
 
 #define TA_NAME		"tcta.ta"
 
@@ -9,6 +13,38 @@
 
 #define TA_UUID         TA_TRUSTCORE_UUID
 
+#define TC_SHMEM_TEE_SUB_ID                       0
+#define TC_NOTIFY_DATA_TEE_SUB_ID                 1
+#define TC_POLL_DATA_TEE_SUB_ID                   2
+
+
+typedef struct _tc_mempool_buf tc_mempool_buf_t;
+
+struct _tc_mempool_buf {
+    uint8_t data[1600];
+    tc_mempool_buf_t *next;
+};
+
+typedef struct {
+    tc_mempool_buf_t *head;
+    tc_mempool_buf_t *tail;
+} tc_mempool_t;
+
+typedef struct {
+    tc_mempool_buf_t *front;
+    tc_mempool_buf_t *back;
+} tc_queue_t;
+
+#define MAX_SHMEM_POOL_SIZE 20
+
+typedef struct {
+    uint8_t tee_data_ready;
+    tc_queue_t ree_tee_q;
+    tc_queue_t tee_ree_q;
+    tc_mempool_t ree_tee_mempool;
+    tc_mempool_buf_t ree_tee_mempool_buf[MAX_SHMEM_POOL_SIZE];
+}tc_shmem_t;
+
 uint32_t trustcore_smc_handler(uint8_t sub_id, uint64_t arg1,
                                uint64_t arg2, uint64_t arg3)
 {
@@ -17,7 +53,30 @@ uint32_t trustcore_smc_handler(uint8_t sub_id, uint64_t arg1,
     EMSG("arg1 0x%"PRIx64, arg1);
     EMSG("arg2 0x%"PRIx64, arg2);
     EMSG("arg3 0x%"PRIx64, arg3);
+    
+    /* Virtual address of the shared memory */
+    vaddr_t *va;
+    tc_shmem_t *tct_shm;
 
+    switch(sub_id)
+    {
+	    case TC_SHMEM_TEE_SUB_ID:
+		    va = (vaddr_t *)core_mmu_add_mapping(MEM_AREA_RAM_NSEC,arg1,sizeof(tc_shmem_t));
+		    if (!va)
+		    {
+	              EMSG("Bad arg address 0x%"PRIxPA, arg1);
+		      return OPTEE_SMC_RETURN_EBADADDR;
+		    }
+                    tct_shm = (tc_shmem_t *)va;
+		    EMSG("TCT Shared memory at 0x%" PRIx64 "  ", tct_shm);
+		    break;
+	    case TC_NOTIFY_DATA_TEE_SUB_ID:
+		    break;
+	    case TC_POLL_DATA_TEE_SUB_ID:
+		    break;
+            default:
+		    return OPTEE_SMC_RETURN_EBADCMD; 
+    }
     return 0;
 }
 
